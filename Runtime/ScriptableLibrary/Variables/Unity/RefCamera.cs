c298637bb2b1b4fb3af181d67c7d465e0d852ef0
Alejandro Güereca <ale_guereca_valdivia@hotmail.com>
2021-08-02 16:52:01 -0500

    Added camera ref variable

---
+++ Runtime/ScriptableLibrary/Variables/Unity/RefCamera.cs
@@ -0,0 +1,5 @@
+namespace Vocario.Scriptable
+{
+    [UnityEngine.CreateAssetMenu(fileName = "Camera_", menuName = "Scriptable/Variables/Camera", order = 11)]
+    public class RefCamera : RefT<UnityEngine.Camera> { }
+}
